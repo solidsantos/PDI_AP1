9408694e3dd58cddef60fb720a19c249b16c0405
Victor Santos <victor.eas19@gmail.com>
2024-07-02 22:33:39 -0300

    :sparkles: working on a new feature

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const router = require('./router');
+// TODO To add environment variables
+const PORT = 8080;
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
@@ -10,5 +12,5 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use('/', router);
 
-app.listen(8080);
-console.log('the server ir running on port:8080!');
+app.listen(PORT);
+console.log(`the server ir running on http://localhost:${8080}/`);
